9cddb5600fb301ca38065482b706a6f395cc39bc
seervi-mandira <mandirao186@gmail.com>
2025-08-02 18:07:59 +0530

    fix: added base for GitHub Pages

--- Clinet/vite.config.js
+++ Clinet/vite.config.js
@@ -1,7 +1,13 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/FullStack/', // 👈 important for GitHub Pages
   plugins: [react()],
 })
